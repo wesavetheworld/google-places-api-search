4e10964eebd296aa4c9ccb85f3f0c3ea683e65d0
marinamarina <shchukina.marina@gmail.com>
2013-10-03 22:41:08 +0100

    Code cleaned up

--- lib/GooglePlacesSearch.php
+++ lib/GooglePlacesSearch.php
@@ -1,82 +1,80 @@
 <?php
 class GooglePlacesSearch {
 	
-	protected $locationsFile = 'input/set_of_search_areas.csv'; //file containing set of searched locations
-	protected $areas = array(); //an array holding the content of the locations .csv file
-	protected $file = 'output/this.txt';
-	protected $apiKey;
+	protected $searched_areas_source_file = 'input/set_of_searched_areas.csv';
+	protected $set_of_searched_areas = array();
+	protected $output_file = 'output/this.txt';
+	protected $api_key;
     protected $query;
-    private $linesCount = 0;
+    private $lines_count = 0;
+    private $lng_step = 0.5;
+    private $lat_step = 0.6;
 
-		
-	//constructor
-	public function __construct($apiKey="your_default_key", $query="pascal") {
-		$this->apiKey = $apiKey;
-        $this->query = $query;		
-		$this->initSetup();
-	}
-
-	//setting up the memory and encoding
-	public function initSetup() {
+	/*
+     * Constructor
+     */
+	public function __construct($api_key="your_default_key", $query="gogh") {
 		ini_set('memory_limit', '512M');
-		$this->ary[] = "UTF-8";
-		$this->ary[] = "ASCII";
-		$this->ary[] = "EUC-JP";
-		mb_detect_order($this->ary);
+        $this->ary[] = "UTF-8";
+        $this->ary[] = "ASCII";
+        $this->ary[] = "EUC-JP";
+        mb_detect_order($this->ary);
+        $this->api_key = $api_key;
+        $this->query = $query;		
 	}
 
-	//get coordinates from the locations file (input folder), save into an array
-    public function getAreas() {
-       $csvData = file_get_contents($this->locationsFile);  
+	/* 
+     * Get coordinates from the locations file
+     * input/set_of_searched_areas.csv, saves it into an array
+     */
+    protected function getAreas() {
+       $csvData = file_get_contents($this->searched_areas_source_file);  
        $csvNumColumns = 5;
        $csvDelim = ",";
-       $areas = array_chunk(str_getcsv($csvData, $csvDelim), $csvNumColumns);       
-       return $areas; 
+       $set_of_searched_areas  = array_chunk(str_getcsv($csvData, $csvDelim), $csvNumColumns);
+       return $set_of_searched_areas; 
     }
 
-
-    //curl request to Google API
-	public function curl_request($url){
+    protected function curl_request_to_google_api($url){
 		    $ch = curl_init($url);
             curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, TRUE);
             curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-            //curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
-            //curl_setopt($ch, CURLOPT_PROXY, 'tcp://10.152.4.180');
-            //curl_setopt($ch, CURLOPT_PROXYPORT, 80);
             $curl_result = curl_exec($ch);
             if ($curl_result === false) echo curl_error($ch);
             curl_close($ch);
             return $curl_result;
 	}
 
-    //Checking, whether there are any results, 
-    //eg., Are there any places named by Mandela(within this pair of coordinates+this radius?)
-    public function handleResults($output, $currentArea) {
+    /* 
+     * Checking whether there are any results, 
+     * eg., are there any places named by Van Gogh
+     * (within this pair of coordinates+this radius?)
+     */
+    protected function handleResults($output, $currentArea) {
             if($output['status'] === "OK") {                 
-                $this->fetchData($output, $currentArea);
+                $this->fetch_data_from_curl($output, $currentArea);
                 echo "I have results!" . "\n";
             }
             else if ($output['status'] === "ZERO_RESULTS") {
-                echo "Nae results!" . "\n";
+                echo "No results!" . "\n";
             } else {
                 echo $output['status'];
             }
     }
 
-    //add the xls file header
-    public function addOutputHeader() {
-        file_put_contents($this->file, "area".","."name".","."lat".","."long".","."id".","."vicinity"."," ."type". "\r\n", FILE_APPEND | LOCK_EX);
+    protected function add_output_header() {
+        file_put_contents($this->output_file, "area".","."name".","."lat".","."long".","."id".","."vicinity"."," ."type". "\r\n", FILE_APPEND | LOCK_EX);
     }
-    //fetch the data from cURL
-    public function fetchData($output, $currentArea) {
+    
+    protected function fetch_data_from_curl($output, $currentArea) {
             $types = array();
             
-            for ($i = 0; $i < sizeof($output['results']); $i++) {
-                $this->linesCount++;
+            for ($i = 0; $i < count($output['results']); $i++) {
+                $this->lines_count++;
 
                 //creating a line
-                $line = " " . $this->linesCount. ","; //add the count column
+                $line = " " . $this->lines_count. ","; //add the count column
                 $line = " " . $currentArea. ","; //add the area column
                 
                 //adding data from the cURL request to the line
@@ -99,50 +97,48 @@ class GooglePlacesSearch {
                 } else {
                     $line .= " " . ",";
                 }
-                //echo $this->encode($line) . "\r\n";
-                file_put_contents($this->file, $this->encode($line) . "\r\n", FILE_APPEND | LOCK_EX);
+                file_put_contents($this->output_file, $this->encode_line($line) . "\r\n", FILE_APPEND | LOCK_EX);
             }
-    } //end of fetch data
+    }
 
-    //encode the line
-    public function encode($line) {
+    protected function encode_line($line) {
         $encoded_line = mb_convert_encoding($line, "UTF-8", "auto");
         return $encoded_line;
     } 
 
-    //main function
+  
     public function Search() {
         $count = 0;
-    	$areas = $this->getAreas();
+    	$set_of_searched_areas = $this->getAreas();
 
         //header
         echo "Lat:" . " - " . "Long:" . "\n"; //add header to the console output
-    	$this->addOutputHeader();             //add header to the xls output
+    	$this->add_output_header();           //add header to the xls output
 
-    	foreach ($areas as $area) {
+    	foreach ($set_of_searched_areas as $area) {
             $currentArea = trim($area[0]);
             $lat = $area[1];	
         	$lngStart = $area[2];
-        	$lng = $lngStart; //-12; //$lat and $lng are the starting values of long (Coordinates for top Left corner) 
-        	$latEnd = $area[3]; //40; 
-        	$lngEnd = $area[4]; //25;	
+        	$lng = $lngStart; //$lat and $lng are the starting values of long (Coordinates for top Left corner) 
+        	$latEnd = $area[3]; 
+        	$lngEnd = $area[4];	
 
             while ($lat > $latEnd ) { 
         	   	$lng = $lngStart; //reset longitude
 
                while($lng < $lngEnd ) { 
 
-                    $url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={$lat},{$lng}&radius=50000&name={$this->query}&language=en-GB&sensor=true&key={$this->apiKey}";
+                    $url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={$lat},{$lng}&radius=50000&name={$this->query}&language=en-GB&sensor=true&key={$this->api_key}";
 
-                    $output = json_decode($this->curl_request($url), true);
+                    $output = json_decode($this->curl_request_to_google_api($url), true);
                     echo ' area: ' . $currentArea . ' | ' . 'lat: ' . $lat . ' | long: ' . $lng . ' | ';
 
                     $this->handleResults($output, $currentArea);
 
-                    $lng = $lng + 0.5; //lng step
+                    $lng = $lng + $this->lng_step;
                 }
-                $lat = $lat - 0.6; //lat step!       
+                $lat = $lat - $this->lat_step;       
             }
-    } //end of foreach
+        } //end of foreach
     } //end of Search()
 }
