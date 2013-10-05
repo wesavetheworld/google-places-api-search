53060b1ef716df38403029eb112ff52afbd25aa1
Marina Shchukina <shchukina.marina@gmail.com>
2013-10-05 12:13:59 +0100

    Update search.php

--- search.php
+++ search.php
@@ -1,10 +1,11 @@
 <?php
 	//Searching for places named after a certain person, for example 
 	// Van Gogh
+	// Please provise your Google Places API key when instantiating a new instance of GooglePlacesSearch object
 
 	require('lib/GooglePlacesSearch.php');
 
 	// Keep in mind that the coordinates provided in the input file should be set as google.maps.LatLng objects
-	$van_gogh_search = new GooglePlacesSearch('AIzaSyDFdGgXfnlFQZBwght6Ho_3jKudlTWkZkY', 'gogh');
+	$van_gogh_search = new GooglePlacesSearch('your_API', 'gogh');
 	
 	$van_gogh_search->Search();
